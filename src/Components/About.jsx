95a734f9405e13215391506b0a4efece5625e844
MostafizurRahman199 <fardilshifat199@gmail.com>
2024-10-24 23:12:03 +0600

    done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='p-4 bg-purple-200 rounded-lg justify-around my-8'>
+     <p> The shopping cart on our e-commerce site is designed to provide a seamless and efficient shopping experience for users. It allows customers to add, remove, and adjust the quantities of products they wish to purchase, displaying real-time updates on the total cost, including discounts, taxes, and shipping. The system ensures that prices are displayed accurately, including support for showing up to three decimal places for precision in pricing.</p>
+
+     
+    </div>
   )
 }
